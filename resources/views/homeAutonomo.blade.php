69eb70d1179bceaae20a4a6979aecac44cc3eea6
Diego Thomas <diegorafael19977@gmail.com>
2018-11-15 14:16:25 -0200

    correção de bugs

--- resources/views/homeAutonomo.blade.php
+++ resources/views/homeAutonomo.blade.php
@@ -8,7 +8,7 @@
 <div class="fundo-home">
         <div class="trabalhador-autonomo">
     <h1 class="h1-homeCliente" >Seja um profissional <br />conosco.</h1>                                                   
-    <p class="p-homeCliente" >- Cadastre-se suas informações<br />- Diga que tipo de serviço você realiza<br />- Aceite pedidos de clientes<br />- Seja avaliado e receba credibilidade  <br />    entre os clientes </p> 
+    <p class="p-homeCliente" >- Cadastre-se suas informações<br />- Diga que tipo de serviço você realiza<br />- Aceite pedidos de clientes<br />- Seja avaliado e receba credibilidade  <br />    entre os clientes </p><br />
 </div>
 
 @endsection
